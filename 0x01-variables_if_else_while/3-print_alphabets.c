7749ddc1f60938821afd70541be00c7506283ce5
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-12 11:07:37 +0000

    if, else, while task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 if success
+ */
+int main(void)
+{
+char a = 'a';
+char A = 'A';
+int n = 0;
+
+while (n < 52)
+{
+if (n <26)
+{
+putchar(a);
+a++;
+}
+
+else
+{
+putchar(A);
+A++;
+}
+n++;
+}
+putchar('\n');
+return (0);
+}
