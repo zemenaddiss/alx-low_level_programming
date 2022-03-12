989d6dd4ce479616740f5c7131f99cc05e1554f3
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-12 11:50:29 +0000

    if, else, while tasks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 if success
+ */
+int main(void)
+{
+char ch = 'a';
+int i = 0;
+while (i < 16)
+{
+if (i < 10)
+{
+putchar(i + '0');
+}
+
+else
+{
+putchar(ch);
+ch++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
