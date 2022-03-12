c314c2e980ba71fbe9ef5dd6a32160183a05eb6f
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-12 11:35:37 +0000

    if, else, while task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *Return: 0 if success
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
