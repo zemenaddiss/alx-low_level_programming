4de6e33ac7a00fcbc6d40c381603df901247ef4f
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-12 07:19:39 +0000

    programming task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 printf("Size of a char: Id byte(s)\n", sizeof(char));
 printf("Size of an int: Id byte(s)\n", sizeof(int));
-printf("Size of a long int: Id byte(s), sizeof(long int));
-printf("Size of a long long int: Id byte(s), sizeof(long long int));
-printf("Size of a float: Id byte(s), sizeof(float));
+printf("Size of a long int: Id byte(s)\n", sizeof(long int));
+printf("Size of a long long int: Id byte(s)\n", sizeof(long long int));
+printf("Size of a float: Id byte(s)\n", sizeof(float));
 return (0);
 }
