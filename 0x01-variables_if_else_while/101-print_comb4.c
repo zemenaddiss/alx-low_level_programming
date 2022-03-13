2b2c5e75dc80a3d3701f754ea270a7aa62a65290
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-13 08:45:00 +0000

    if, else, while advance task 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,7 @@ for (k = j + 1; k <= 9; k++)
 putchar(i + '0');
 putchar(j + '0');
 putchar(k + '0');
-if ((( i * 100) + (j * 10) + k) < 789)
+if (((i * 100) + (j * 10) + k) < 789)
 {
 putchar(',');
 putchar(' ');
