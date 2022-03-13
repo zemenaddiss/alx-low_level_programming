2e76ba6b43a7e5e237b55dbeec38d92d651da2ef
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-13 08:40:30 +0000

    if, else, while advance task 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 if success
+ */
+int main(void)
+{
+int i, j, k;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if ((( i * 100) + (j * 10) + k) < 789)
+{
+putchar(',');
+putchar('');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
