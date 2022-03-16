6a6d550e79349b1f5a8ce9dd4d5b1e679a074a5e
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 11:59:44 +0000

    nested loop advance task 4

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - Sum even Fibonacci less than 4000,000
+ *
+ *Return: none
+ */
+int main(void)
+{
+unsigned long cout, k;
+unsigned long i = 0;
+unsigned long sum = 0;
+unsigned long j = 1;
+for (count = 0; count < 50; count++)
+{
+k = i + j;
+i = j;
+j = k;
+if (k % 2 == 0 && k < 4000000)
+{
+sum += k;
+}
+
+}
+printf("%lu\n", sum);
+return (0);
+}
