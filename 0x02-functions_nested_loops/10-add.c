e427ddd82606375e855b14310cd8c58be4eef058
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 09:26:31 +0000

    nested loop task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *add - Print addition result
+ *@i: print int i
+ *@j: print int j
+ *Return: 0 for success
+ */
+int add(int i, int j)
+{
+return (i + j);
+}
