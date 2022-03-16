e79b7668a9712c3ef3b41adf9ac355bfdcd788c5
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 06:46:34 +0000

    nested loop task 1

---
+++ 0x02-functions_nested_loops/1-alpabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print alphabet lowercase
+ *
+ *Return: 0 if success
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
