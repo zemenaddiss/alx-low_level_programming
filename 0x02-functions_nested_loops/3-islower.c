14ac8d48aac94b246915beb612a20205d210d1b0
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 07:32:47 +0000

    nested loop task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_islower -  ckeck lowercase character
+ *@c: The input character to check
+ *Return: int.
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
