77480f5860fb307b91866eac3c5ac925b495609b
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 09:59:13 +0000

    nested loop advanc task 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/** 
+ *print_times_table -Function to print table
+ *@n: input of table
+ *Return: empty
+ */
+void print_times_table(int n)
+{
+int a, b;
+int c = 0;
+for (n <= 15 && n >= 0)
+{
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+c = (a * b);
+if (c < 10)
+{
+if (b != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+_putchar(c + '0');
+}
+else if (c < 100)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar((c / 100) + '0');
+_putchar(((c % 100) / 10) + '0');
+_putchar((c % 10) + '0');
+}
+if (b != n)
+_putchar(',');
+}
+_putchar('\n);
+}
+}
+}
