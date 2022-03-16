1af039af13626cd7d93e7979dd0995c8914c3443
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 14:38:01 +0000

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,5 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int m, int d);
-int print_remaining_days(int m, int d, int y);
+void print_remaining_days(int m, int d, int y);
 #endif
