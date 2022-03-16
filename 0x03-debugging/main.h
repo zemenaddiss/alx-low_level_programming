8a05dbee27080ff0f54c39b1c4d843edd4df9091
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 14:17:32 +0000

    main file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int m, int d);
+int print_remaining_days(int m, int d, int y);
+#endif
