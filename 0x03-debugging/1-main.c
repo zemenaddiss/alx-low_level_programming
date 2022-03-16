11f8d94110e24121f7a2a20125160910e71b7552
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 14:51:44 +0000

    debugging task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,11 @@ printf("infinite loop incoming :(\n");
 
 i = 0;
 /*
- * while (i < 10)
- * {
- * Infinite loop - No increase of variabl
- * putchar(i);
- * }
+ *while (i < 10)
+ *{
+ *Infinite loop - No increase of variabl
+ *putchar(i);
+ *}
  */
  printf("Infinite loop avoided! \\o/\n");
  return (0);
