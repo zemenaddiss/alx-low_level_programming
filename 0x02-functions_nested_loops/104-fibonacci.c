b9d0f39467e2dd7eed2a0d448610a3b80efe4634
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 12:33:29 +0000

    nested  loop advance task 4

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ *main - The first 98 Fibonacci numbers
+ *Return: none
+ */
+int main(void)
+{
+int ct;
+unsigned long i, j, k, m, n, p, c;
+ct = 0;
+i = 0;
+j = 1;
+for (ct = 1; ct <= 98;
+ct++)
+{
+k = i + j;
+i = j;
+j = k;
+printf("%lu, ", k);
+}
+m = i % 1000;
+i = i / 1000;
+n = j % 1000;
+j = j / 1000;
+while (ct <= 98)
+{
+c = (m + n) / 1000;
+p = (m + n) - c * 1000;
+k = (i + j) + c;
+m = n;
+n = p;
+i = j;
+j = k;
+if (p >= 100)
+printf("%lu%lu", k, p);
+else
+printf("%lu0%lu", k, p);
+if (ct != 98)
+printf(",");
+ct++;
+}
+putchar('\n');
+return (0);
+}
