5b7b7dfa8d9f2fd06be094bacb1f1022a25ad2ce
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 08:47:29 +0000

    nested loop task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *jack_bauer - Prints hours of a day
+ *
+ *Return: 0 if success
+ */
+int jack_bauer(void)
+{
+int a, b;
+for (a = 0; a < 24; a++)
+{
+for (b = 0; b < 60; b++)
+{
+_putchar(a / 10 + '0');
+_putchar(a % 10 + '0');
+_putchar(':');
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
+_putchar('\n');
+}
+}
+}
