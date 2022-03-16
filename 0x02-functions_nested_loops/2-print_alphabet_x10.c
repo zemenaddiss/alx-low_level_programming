c3b80c1e3b16d5e865fcdb86de3c3da5c651fd37
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-16 07:22:19 +0000

    nested loop task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *print_alphabet_x0 - print alphabet
+ *print_alphabet_x10 - print alphabet
  *
  *Return: Always 0 if success
  */
-void (print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 int i;
 int j = 0;
