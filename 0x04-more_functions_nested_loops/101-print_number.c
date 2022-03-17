03cbdc23800d4436c908d79b96455ccaf3e29bde
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-17 15:16:44 +0000

    more nested loop advance task 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@ int i;
 int k = 1;
 if (n < 0)
 {
-n = n * (-1);
+n = n * -1;
 _putchar('-');
 }
 
@@ -26,11 +26,14 @@ while (n1 >= 10)
 k = k * 10;
 n1 = n1 / 10;
 }
+
 _putchar((n2 / k) + '0');
 k = k / 10;
+
 for (i = k; i >= 1; i = i / 10)
 _putchar((n2 / i) % 10 + '0');
 }
+
 else
 _putchar(n1 + '0');
 }
