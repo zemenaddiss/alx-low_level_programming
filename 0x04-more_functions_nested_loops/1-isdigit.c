0edf9d9e7465f90d047fbd76e2c3b064679785d4
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-17 09:34:07 +0000

    more nested loop task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _isdigit - Checks for digit
+* @c: input value
+* Return: int
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+return (0);
+}
