0011526f5ed59da8cee0e37ec5d7b977d616d561
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-17 10:30:05 +0000

    more nested loop task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_diagonal -draws diagonal line
+* @n: input value
+* Return: none
+*/
+void print_diagonal(int n)
+{
+int i = 0;
+int j = 0;
+while (i < n)
+{
+while (j <= i)
+{
+if (j != i)
+_putchar(' ');
+else
+_putchar(92);
+j += 1;
+}
+_putchar('\n');
+i += 1;
+j = 0;
+}
+if (n <= 0)
+_putchar('\n');
+}
