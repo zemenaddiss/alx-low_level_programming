8617d8ae3703e34f8812c822d7f98c76f39736ed
zemenaddiss <99796576+zemenaddiss@users.noreply.github.com>
2022-03-21 11:35:12 -0300

    pointer, array and string task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,17 +4,17 @@
  * _puts - prints a string
  *
  * @str: pointer to char
- * Return: none
+ * Return: nothing
  */
 
 void _puts(char *str)
 {
 	int k = 0;
-	
+
 	while (str[k] != '\0')
 	{
-	       _putchar(str[k]);
-	       k += 1;
+		_putchar(str[k]);
+		k += 1;
 	}
 	_putchar('\n');
 }
