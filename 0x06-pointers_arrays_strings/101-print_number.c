9ea99c703307a9d55cbc4f5f6780181f4194ed12
zemenaddiss <99796576+zemenaddiss@users.noreply.github.com>
2022-03-24 09:59:28 -0300

    more pointer, array and string advance task 2

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
+
+void print_number(int n)
+{
+	unsigned int k;
+
+	k = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		k = -n;
+	}
+
+	if (k / 10 != 0)
+	{
+		print_number(k / 10);
+	}
+	_putchar((k % 10) + '0');
+}
