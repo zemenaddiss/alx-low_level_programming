9aecb0c1bc360159247cd25445c04e2208ee5788
zemenaddiss <18zemenaddiss@gmail.com>
2022-03-24 07:20:53 +0000

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
  * _puts - prints a string
  *
  * @str: pointer to char
  * Return: none
  */
-
 void _puts(char *str)
 {
 	int k = 0;
+
 	while (str[k] != '\0')
 	{
-	_putchar(str[k]);
-		k += 1;
+
+        	_putchar(str[k]);
+	      	k += 1;
 	}
 	_putchar('\n');
 }
