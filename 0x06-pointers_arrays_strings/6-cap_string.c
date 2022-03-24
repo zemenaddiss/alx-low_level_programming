79398d800ee173cc718eaf961df3401409117367
zemenaddiss <99796576+zemenaddiss@users.noreply.github.com>
2022-03-24 07:57:52 -0300

    more pointer, array and string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: Pointer to Char
+ * Return: char.
+ */
+
+
+int separator(char c)
+{
+switch (c)
+{
+case ' ':
+case '\t':
+case '\n':
+case ',':
+case ';':
+case '.':
+case '!':
+case '?':
+case '"':
+case '(':
+case ')':
+case '{':
+case '}':
+return (1);
+default:
+return (0);
+}
+}
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to uppercase
+ * Return: returns the modified string
+ */
+char *cap_string(char *s)
+{
+int count, upper;
+upper = -32;
+count = 0;
+while (s[count] != '\0')
+{
+if (s[count] >= 'a' && s[count] <= 'z')
+{
+if (s[count] == *s || separator(s[count - 1]))
+s[count] += upper;
+}
+count++;
+}
+return (s);
+}6
