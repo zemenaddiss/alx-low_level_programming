7245bb8185bbc86ae714ebaae12d81c1a17d25dd
zemenaddiss <99796576+zemenaddiss@users.noreply.github.com>
2022-03-30 09:04:25 -0300

    Recursion task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *factorial - factorial of a given number
+ *@n: pointer block of memory
+ *Return: factorial
+ */
+int factorial(int n)
+{
+if (n == 0)
+return (1);
+else if (n < 0)
+return (-1);
+else
+return (n * factorial(n - 1));
+}
