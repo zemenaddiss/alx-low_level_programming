041c488a40251b795fc294aaec19dc135912aeb6
zemenaddiss <99796576+zemenaddiss@users.noreply.github.com>
2022-04-01 06:59:55 -0300

    argc and argv task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the program name
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
