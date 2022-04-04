2a9b49d1a73693efbf9fce2cf183c4f773699074
zemenaddiss <99796576+zemenaddiss@users.noreply.github.com>
2022-04-04 07:45:25 -0400

    malloc and free task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * free_grid - allocates a grid
+ * free_grid - allocates a grid, make space and free space
  * @grid: takes in width of grid
  * @height: height of grid
  * Return: free grid
  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
