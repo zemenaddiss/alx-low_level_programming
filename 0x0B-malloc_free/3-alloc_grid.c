5a3411156ebcc645e3ccf2195b6b93009a7a6c11
zemenaddiss <99796576+zemenaddiss@users.noreply.github.com>
2022-04-04 07:43:23 -0400

    malloc and free task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * alloc_grid - allocates a grid
+ * alloc_grid - allocates a grid, make space and free space
  * @width: takes in width of grid
  * @height: height of grid
  * Return: grid with freed spaces
  */
+
 int **alloc_grid(int width, int height)
 {
-
+/*Declaring variables*/
 int **grid;
 int i, j;
 
@@ -16,7 +17,7 @@ if (width <= 0 || height <= 0)
 return (NULL);
 }
 
-grid = malloc(sizeof(int *)
+grid = malloc(sizeof(int *) * height); /*malloc*/
 
 if (grid == NULL)
 {
